aed19d3597413548f0da6cc6f276186dfb595029
horus <smonsalve@gmail.com>
2013-10-07 20:21:59 -0500

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paisahuevo::Application.config.secret_key_base = '7a14463a443c242b1f8e30ba342bc6f67d85befdab330a2b3e6846f5eda5ba65ddfbb2f504bd3187c341467e6ba03cc365a875c4f8e90ecdc9883a02b802058c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paisahuevo::Application.config.session_store :encrypted_cookie_store, key: '_paisahuevo_session'
